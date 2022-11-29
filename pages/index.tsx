71c811bae88630675ded702c72b130ae518eed0e
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-11-29 00:41:57 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
@@ -20,6 +21,9 @@ export default function Home() {
       </section>
 
       {/* About */}
+      <section id='about'>
+        <About />
+      </section>
 
       {/* Experience */}
       
