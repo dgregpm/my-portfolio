69514087d2cfa06e8c86006804ba8ee6e8a157ba
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-11-29 00:41:47 -0500

    Update BackgroundCircles.tsx

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -20,8 +20,8 @@ const BackgroundCircles = (props: Props) => {
                 duration: 2.5,
             }}
             className='relative flex justify-center items-center'>
-            <div className={`absolute border border-[#333333] rounded-${ full ? 'full' : '[65px]'} h-[200px] w-[200px] mt-52 animate-ping`} />
-            <div className={`absolute border border-[#333333] rounded-${ full ? 'full' : '[100px]'} h-[300px] w-[300px] mt-52`} />
+            {/* <div className={`absolute border border-[#333333] rounded-${ full ? 'full' : '[65px]'} h-[200px] w-[200px] mt-52 animate-ping`} /> */}
+            {/* <div className={`absolute border border-[#333333] rounded-${ full ? 'full' : '[100px]'} h-[300px] w-[300px] mt-52`} /> */}
             <div className={`absolute border border-[#333333] rounded-${ full ? 'full' : '[165px]'} h-[500px] w-[500px] mt-52`} />
             <div className={`absolute border border-[#F7AB0A] rounded-${ full ? 'full' : '[215px]'} h-[650px] w-[650px] opacity-20 mt-52 animate-pulse`} />
             <div className={`absolute border border-[#333333] rounded-${ full ? 'full' : '[265px]'} h-[800px] w-[800px] mt-52`} />
