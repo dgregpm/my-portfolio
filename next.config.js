9ab6fb087e45c1fd16bcfc9adf8bb0b91c7f3269
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-02 11:01:50 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['links.papareact.com', 'images.g2crowd.com'],
+  },
+  // images: {
+  //   remotePatterns: [{
+  //     protocol: 'https',
+  //     hostname: 'images.g2crowd.com',
+  //     port: '',
+  //     pathname: '/',
+  //   }]
+  // }
 }
 
 module.exports = nextConfig
