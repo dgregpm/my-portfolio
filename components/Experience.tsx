07f503c546e3f1d4d63d24ea4adce5f4c45572a9
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-02 11:01:25 -0500

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ExperienceCard from './ExperienceCard'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 const Experience = (props: Props) => {
   return (
-    <div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row 
+    <div className='h-screen flex relative overflow-hidden flex-col text-left  
     max-w-full px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] ml-[10px] text-gray-500 text-2xl '>
+        <motion.h3 
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            transition={{duration: 1.5}}
+            className='uppercase tracking-[20px] ml-[10px] text-gray-500 text-2xl mt-24 '>
             Experience
-        </h3>
-        <div className="">
+        </motion.h3>
+        <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scroll-fresh">
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
