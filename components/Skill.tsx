95e6b93ff9089ffdeaa29f0f1e3ea26b3ec1b736
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-02 11:01:43 -0500

    Create Skill.tsx

---
+++ components/Skill.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+
+type Props = {
+    directionLeft?: boolean;
+}
+
+const Skill = ( directionLeft: Props) => {
+  return (
+    <motion.div
+        initial={{
+            x: directionLeft ? -200 : 200
+        }}
+        transition={{ duration:1}}
+        whileInView={{opacity:1, x:0}}
+        className='group relative flex cursor-pointer'
+    >
+        <Image 
+            src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png"
+            alt=''
+            width={100}
+            height={100}
+            className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:h-32 xl:w-32 
+            filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='rounded-full absolute opacity-0 group-hover:opacity-60 transition duration-300
+        ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 z-0'>
+            <div className='flex items-center justify-center h-full'>
+                <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+            </div>
+        </div>
+    </motion.div>
+  )
+}
+
+export default Skill
