31db13dc12be9a6fb05c57188445d6bbd61b56b2
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-04 23:42:17 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['links.papareact.com', 'images.g2crowd.com'],
+    domains: ['links.papareact.com', 'images.g2crowd.com', 'cdn.sanity.io'],
   },
   // images: {
   //   remotePatterns: [{
