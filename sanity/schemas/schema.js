c7631a95bd865d4c9e52749b270d15912954d9fc
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-04 23:43:30 -0500

    Update schema.js

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,10 +5,14 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
+
+
+import skill from './skill'
+import social from './social'
+import project from './project'
 import pageInfo from './pageInfo'
-import author from './author'
+import experience from './experience'
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,7 +23,11 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
+    skill,
     pageInfo,
+    experience,
+    social,
+    project
    
   ]),
 })
