a838a45c85eb02ecf8d3cccca035e8b919215c1b
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-04 23:42:13 -0500

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import { Skill } from '../typings'
+import { urlFor } from '../sanity'
 
 type Props = {
+    skill: Skill
     directionLeft?: boolean;
 }
 
-const Skill = ( directionLeft: Props) => {
+const Skill = ( {skill, directionLeft}: Props) => {
   return (
     <motion.div
         initial={{
@@ -14,22 +17,23 @@ const Skill = ( directionLeft: Props) => {
         }}
         transition={{ duration:1}}
         whileInView={{opacity:1, x:0}}
-        className='group relative flex cursor-pointer'
+        className='group relative flex items-center gap-x-5 cursor-pointer'
     >
         <Image 
-            src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png"
+            src={urlFor(skill.image).url()}
             alt=''
             width={100}
             height={100}
-            className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:h-32 xl:w-32 
+            className='rounded-full border border-gray-500 object-fit w-24 h-24 md:w-28 md:h-28 xl:h-32 xl:w-32 
             filter group-hover:grayscale transition duration-300 ease-in-out'
         />
         <div className='rounded-full absolute opacity-0 group-hover:opacity-60 transition duration-300
         ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 z-0'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+                <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
             </div>
         </div>
+        <p>{skill.title}</p>
     </motion.div>
   )
 }
