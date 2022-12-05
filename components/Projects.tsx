35ead0d0270fde4345b7e54f017046090805f45c
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-04 23:42:09 -0500

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Project } from '../typings'
+import { urlFor } from '../sanity'
+import Image from 'next/image'
 
-type Props = {}
+type Props = {
+    projects: Project[]
+}
 
-const Projects = (props: Props) => {
-    const projects = [1,2,3,4,5]
+const Projects = ({projects}: Props) => {
+    console.log(projects)
   return (
     <motion.div 
         initial={{opacity:0}}
@@ -16,27 +21,39 @@ const Projects = (props: Props) => {
         </h3>
 
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scroll-fresh">
-            {projects.map((project,i) => (
-                <div key={i} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+            {projects?.map((project,i) => (
+                <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                     <motion.img
                         initial={{opacity:0, y:-300}}
                         whileInView={{opacity:1, y:0}}
                         transition={{duration:1.2}}
 
-                        key={i} src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png" alt="" />
+                        key={i} src={urlFor(project?.image).url()} alt="" />
 
                     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                         <h4 className='text-4xl font-semibold text-center'>
                             <span className='underline decoration-primary/50'>
-                                Case Study {i + 1} of {projects.length}:
+                                Project {i + 1} of {projects.length}:
                             </span>{' '}
-                            UPS CLone
+                            {project?.title}
                         </h4>
-                        <p className='text-lg text-center md:text-left'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis 
-                            saepe quae porro repudiandae vel aperiam? Nulla alias cum accusamus et eaque 
-                            debitis veniam recusandae ipsum ea dolorum, voluptate asperiores repellendus quisquam 
-                            velit! 
-                            </p>
+
+                        <div className='flex items-center space-x-2 justify-center'>
+                        {project?.technologies.map((technology) => (
+                            <Image
+                            src={urlFor(technology.image).url()}
+                            key={technology._id}
+                            alt=''
+                            height={250}
+                            width={250}
+                            className='h-10 w-10'
+                            />
+                        ))}
+                        </div>
+
+                        <p className='text-lg text-center md:text-left'>
+                            {project?.summary}
+                        </p>
                     </div>
                 </div>
 
