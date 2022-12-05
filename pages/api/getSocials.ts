5d7301b545c17ef6951527b906dec146b9cef3da
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-04 23:52:02 -0500

    Update getSocials.ts

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -4,8 +4,10 @@ import { sanityClient } from "../../sanity";
 import { Social } from "../../typings";
 
 const query = groq`
-    *[_type == 'social']
+    *[ _type == "social"]
 `
+
+
 
 type Data = {
     socials: Social[]
